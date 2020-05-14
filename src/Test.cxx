439b1aaeb785fbe52f766d352488555f7bf9dd65
Andrés Bórquez <andres.borquezc@gmail.com>
2020-05-14 12:51:26 -0400

    Started Acceptance and resurrected GetEvents

--- src/Test.cxx
+++ src/Test.cxx
@@ -139,6 +139,7 @@ int main() {
   // ROOT::EnableImplicitMT();
 
   // open tree
+  /*
   ROOT::RDataFrame d("ntuple_sim", "/home/borquez/omegaThesis/out/prunedSim/old/C/pruned_out.root");
 
   auto numero = d.Count();
@@ -177,4 +178,17 @@ int main() {
   
   std::cout << "has omega in gsim: " << poto2 << std::endl;
   std::cout << "has omega in simrec: " << poto3 << std::endl;
+  */
+  /*** 5 ***/
+  // color stuff for 2D plots (from smoran)
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 255;
+  
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  gStyle->SetNumberContours(NCont);
 }
