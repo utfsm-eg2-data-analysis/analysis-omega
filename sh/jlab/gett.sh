177d4bed9f9a707b8405eca1dfb419c7680d447e
Andrés Bórquez <andres.borquezc@gmail.com>
2020-08-26 18:04:50 -0400

    gett: started dev - v1

---
+++ sh/jlab/gett.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+####################################################################################################################################
+# ./gett.sh <tape file> <output dir>                                                                                               #
+#     both parameters must be absolute locations                                                                                   #
+#                                                                                                                                  #
+# EG: ./gett.sh /mss/clas/eg2a/production/Simulation/omega_lepto/D/43/recsisD_01.root /home/borquez/volatile/omegaSim/output/D/43/ #
+####################################################################################################################################
+
+# Script to get files from tape as auger jobs
+# (exclusive for my simulation files!)
+
+#####
+# Input
+###
+
+inputArray=("$@")
+
+INFILE="${inputArray[0]}"
+OUDIR="${inputArray[1]}"
+
+#####
+# Main
+###
+
+# set env
+source ~/.bashrc
+cd ${PRODIR}
+
+XMLDIR=${PRODIR}/xml/gett/
+
+# declaration of variables
+#jobfile=
+jobemail="andres.borquez.14@sansano.usm.cl"
+jobproject="eg2a"
+jobtrack="debug" # "debug" or "analysis"
+jobos="general"
+jobtime="1" # hour
+jobspace="1" # GB
+jobmemory="1" # GB
+
+# strings processing
+basename="${INFILE##*/}"
+tarName="${basename#recsis}"
+tarName="${tarName%_*}"
+runNumber="${basename%.root}"
+runNumber="${runNumber##*_}"
+dirname="${INFILE%/*}"
+nDir="${INFILE##*/}"
+
+jobname="gett_${tarName}-${nDir}_${runNumber}"
+jobfile="${XMLDIR}/${jobname}.xml"
+
+echo ${jobname}
+
+echo "<Request>"                                                            > ${jobfile}
+echo "  <Email email=\"${jobemail}\" request=\"true\" job=\"true\"/>"      >> ${jobfile}
+echo "  <Project name=\"${jobproject}\"/>"                                 >> ${jobfile}
+echo "  <Track name=\"${jobtrack}\"/>"                                     >> ${jobfile}
+echo "  <OS name=\"${jobos}\"/>"                                           >> ${jobfile}
+echo "  <Name name=\"${jobname}\"/>"                                       >> ${jobfile}
+echo "  <TimeLimit time=\"${jobtime}\" unit=\"hours\"/>"                   >> ${jobfile}
+echo "  <DiskSpace space=\"${jobspace}\" unit=\"GB\"/>"                    >> ${jobfile}
+echo "  <Memory space=\"${jobmemory}\" unit=\"GB\"/>"                      >> ${jobfile}
+echo "  <CPU core=\"1\"/>"                                                 >> ${jobfile}
+# set input
+echo "  <Input src=\"mss:${INFILE}\" dest=\"${basename}\"/>"               >> ${jobfile}
+# command empty
+echo "  <Command><![CDATA["                                                >> ${jobfile}
+echo "    ls -lrth ${basename}"                                            >> ${jobfile}
+echo "  ]]></Command>"                                                     >> ${jobfile}
+# set outputs
+echo "  <Output src=\"${basename}\" dest=\"${OUDIR}/${basename}\"/>"       >> ${jobfile}
+echo "  <Stdout dest=\"${XMLDIR}/${jobname}.out\"/>"                       >> ${jobfile}
+echo "  <Stderr dest=\"${XMLDIR}/${jobname}.err\"/>"                       >> ${jobfile}
+echo "  <Job>"                                                             >> ${jobfile}
+echo "  </Job>"                                                            >> ${jobfile}
+echo "</Request>"                                                          >> ${jobfile}
+
+echo "Submitting job: ${jobfile}"
+jsub --xml ${jobfile}
+
