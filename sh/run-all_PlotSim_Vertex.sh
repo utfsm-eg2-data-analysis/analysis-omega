e770d787135de98c45df4f4ad4353f90f075b61b
Andrés Bórquez <andres.borquezc@gmail.com>
2020-09-04 19:45:27 -0400

    Investigating on vertex! -v2

--- sh/run-all_PlotSim_Vertex.sh
+++ sh/run-all_PlotSim_Vertex.sh
@@ -3,61 +3,61 @@
 # run all available PlotSim_Vertex routines!
 
 # old C
-./run_PlotSim_Vertex.sh old C gsim
-./run_PlotSim_Vertex.sh old C simrec
-./run_PlotSim_Vertex.sh old C simrec 0  
-./run_PlotSim_Vertex.sh old C simrec 1  
-./run_PlotSim_Vertex.sh old C simrec 2  
-./run_PlotSim_Vertex.sh old C simrec 3  
-./run_PlotSim_Vertex.sh old C simrec 4  
-./run_PlotSim_Vertex.sh old C simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old C simrec 5 
 
 # old Fe
-./run_PlotSim_Vertex.sh old Fe gsim
-./run_PlotSim_Vertex.sh old Fe simrec
-./run_PlotSim_Vertex.sh old Fe simrec 0  
-./run_PlotSim_Vertex.sh old Fe simrec 1  
-./run_PlotSim_Vertex.sh old Fe simrec 2  
-./run_PlotSim_Vertex.sh old Fe simrec 3  
-./run_PlotSim_Vertex.sh old Fe simrec 4  
-./run_PlotSim_Vertex.sh old Fe simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh old Fe simrec 5 
 
 # usm D
-./run_PlotSim_Vertex.sh usm D gsim
-./run_PlotSim_Vertex.sh usm D simrec
-./run_PlotSim_Vertex.sh usm D simrec 0  
-./run_PlotSim_Vertex.sh usm D simrec 1  
-./run_PlotSim_Vertex.sh usm D simrec 2  
-./run_PlotSim_Vertex.sh usm D simrec 3  
-./run_PlotSim_Vertex.sh usm D simrec 4  
-./run_PlotSim_Vertex.sh usm D simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm D simrec 5 
 
 # usm C
-./run_PlotSim_Vertex.sh usm C gsim
-./run_PlotSim_Vertex.sh usm C simrec
-./run_PlotSim_Vertex.sh usm C simrec 0  
-./run_PlotSim_Vertex.sh usm C simrec 1  
-./run_PlotSim_Vertex.sh usm C simrec 2  
-./run_PlotSim_Vertex.sh usm C simrec 3  
-./run_PlotSim_Vertex.sh usm C simrec 4  
-./run_PlotSim_Vertex.sh usm C simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm C simrec 5 
 
 # usm Fe
-./run_PlotSim_Vertex.sh usm Fe gsim
-./run_PlotSim_Vertex.sh usm Fe simrec
-./run_PlotSim_Vertex.sh usm Fe simrec 0  
-./run_PlotSim_Vertex.sh usm Fe simrec 1  
-./run_PlotSim_Vertex.sh usm Fe simrec 2  
-./run_PlotSim_Vertex.sh usm Fe simrec 3  
-./run_PlotSim_Vertex.sh usm Fe simrec 4  
-./run_PlotSim_Vertex.sh usm Fe simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Fe simrec 5 
 
 # usm Pb
-./run_PlotSim_Vertex.sh usm Pb gsim
-./run_PlotSim_Vertex.sh usm Pb simrec
-./run_PlotSim_Vertex.sh usm Pb simrec 0  
-./run_PlotSim_Vertex.sh usm Pb simrec 1  
-./run_PlotSim_Vertex.sh usm Pb simrec 2  
-./run_PlotSim_Vertex.sh usm Pb simrec 3  
-./run_PlotSim_Vertex.sh usm Pb simrec 4  
-./run_PlotSim_Vertex.sh usm Pb simrec 5 
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb gsim
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 0  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 1  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 2  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 3  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 4  
+sh $PRODIR/sh/run_PlotSim_Vertex.sh usm Pb simrec 5 
