bd2a7cb1ab077221b4bf69133d1f4389a20e1d43
Andrés Bórquez <andres.borquezc@gmail.com>
2020-09-07 13:11:25 -0300

    FNC: rearrangin' jobs - test job, attempt 2

--- sh/jlab/run_FNC.sh
+++ sh/jlab/run_FNC.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-#####
-# Functions
-###
-
-function get_num_2dig()
-{
-  sr=$1
-  srn=""
-  if [[ $sr -lt 10 ]]; then
-    srn="0$sr"
-  else
-    srn="$sr"
-  fi
-  echo $srn
-}
-
-#####
-# Main
-###
-
 # set env
 source /home/borquez/.bashrc
 
@@ -27,7 +7,38 @@ inputOption=
 tarName=
 rn=
 
-for ((counter=1; counter <= 35; counter++)); do
-    scounter=$(get_num_dig2 $counter)
-    ./FilterNCombine${inputOption} -t${tarName} -r${rn}_${scounter}
-done
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_01 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_02 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_03 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_04 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_05 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_06 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_07 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_08 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_09 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_10 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_11 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_12 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_13 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_14 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_15 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_16 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_17 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_18 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_19 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_20 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_21 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_22 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_23 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_24 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_25 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_26 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_27 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_28 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_29 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_30 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_31 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_32 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_33 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_34 
+./FilterNCombine${inputOption} -t${tarName} -r${rn}_35
