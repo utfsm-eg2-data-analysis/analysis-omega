b38b1c113ffb1dc5a09d0691b2a53cee9235449f
Andrés Bórquez <andres.borquezc@gmail.com>
2020-09-07 22:03:30 -0300

    SampFrac: checkin' on simulations - v1

--- src/CheckSampFrac.cxx
+++ src/CheckSampFrac.cxx
@@ -1,34 +1,31 @@
 /**************************************/
-/* PlotSim_SampFrac.cxx               */
+/* CheckSampFrac.cxx                  */
 /*                                    */
 /* Andrés Bórquez                     */
 /*                                    */
 /**************************************/
 
-// plots electron's sampling fraction for simulations
+// plots electron's sampling fraction for data and simulations/reconstructed
 
 #include "analysisConfig.h"
 
 /*** Global variables ***/
 
-TString outDir = proDir + "/out/PlotSim";
+TString outDir = proDir + "/out/CheckSampFrac";
 TString inputFile1 = "";
 TString inputFile2 = "";
 TString inputFile3 = "";
 
 // options
 TString targetOption;
-Int_t dataFlag, simrecFlag, gsimFlag;
+Int_t dataFlag = 0;
+Int_t simrecFlag = 0;
+TString runNumber;
 
-TString dataOption;
 TString titleDraw;
 
-// cuts
-TCut cutAll;
-TCut cutTargType;
-
-TString outPrefix;
 TString plotFile;
+TString rootFile;
 
 /*** Declaration of functions ***/
 
@@ -46,9 +43,6 @@ int main(int argc, char **argv) {
   // dir structure, just in case
   system("mkdir -p " + outDir);
   
-  // setting cuts
-  cutAll = cutDIS && cutPi0 && cutPipPim;
-
   TChain *treeExtracted = new TChain();
   treeExtracted->Add(inputFile1 + "/original");
   treeExtracted->Add(inputFile2 + "/original");
@@ -66,7 +60,7 @@ int main(int argc, char **argv) {
   TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
   
   TH2F *theHist;
-  treeExtracted->Draw("Etote/Pe:Pe>>hist(100, 0., 5, 100, 0.1, 0.4)", cutTargType && statusCuts_electrons, "goff");
+  treeExtracted->Draw("Etote/Pe:Pe>>hist(100, 0., 5, 100, 0.1, 0.4)", statusCuts_electrons, "goff");
   theHist = (TH2F *)gROOT->FindObject("hist");
   
   /*** Drawing ***/
@@ -77,21 +71,22 @@ int main(int argc, char **argv) {
   gStyle->SetOptStat(0);
   gStyle->SetNumberContours(NCont); // colors, smoran
   
-  theHist->SetTitle("Sampling Fraction cut for " + targetOption + titleDraw);
+  theHist->SetTitle("Sampling Fraction cut for D+" + targetOption + titleDraw);
   theHist->GetYaxis()->SetTitle("Etot/P");
   theHist->GetXaxis()->SetTitle("P");
   theHist->GetYaxis()->CenterTitle();
   theHist->GetXaxis()->CenterTitle();
   theHist->SetContour(99);
   theHist->Draw("COLZ");
-
-  // normalization
-  // theHist->Scale(1/((Double_t) theHist->GetEntries()));
-
-  // log scale
-  // gPad->SetLogz(); // important
   
   c->Print(plotFile); // output file
+
+  // create file, write hist and close file
+  if (!dataFlag) {
+    TFile *theFile = new TFile(rootFile, "RECREATE");
+    theHist->Write();
+    theFile->Close();
+  }
   
   return 0;
 }
@@ -101,67 +96,55 @@ int main(int argc, char **argv) {
 inline int parseCommandLine(int argc, char* argv[]) {
   Int_t c;
   if (argc == 1) {
-    std::cerr << "Empty command line. Execute ./PlotSim_SampFrac -h to print usage." << std::endl;
+    std::cerr << "Empty command line. Execute ./CheckSampFrac -h to print usage." << std::endl;
     exit(0);
   }
-  while ((c = getopt(argc, argv, "hg:s:d:")) != -1)
+  while ((c = getopt(argc, argv, "hs:d:r:")) != -1)
     switch (c) {
     case 'h': printUsage(); exit(0); break;
     case 'd': targetOption = optarg; dataFlag = 1; break;
     case 's': targetOption = optarg; simrecFlag = 1; break;
-    case 'g': targetOption = optarg; gsimFlag = 1; break;
+    case 'r': runNumber = optarg; break;
     default:
-      std::cerr << "Unrecognized argument. Execute ./PlotSim_SampFrac -h to print usage." << std::endl;
+      std::cerr << "Unrecognized argument. Execute ./CheckSampFrac -h to print usage." << std::endl;
       exit(0);
       break;
     }
 }
 
 void printUsage() {
-  std::cout << "PlotSim_SampFrac program. Usage is:" << std::endl;
+  std::cout << "CheckSampFrac program. Usage is:" << std::endl;
   std::cout << std::endl;
-  std::cout << "./PlotSim_SampFrac -h" << std::endl;
+  std::cout << "./CheckSampFrac -h" << std::endl;
   std::cout << "  prints help and exit program" << std::endl;
   std::cout << std::endl;
-  std::cout << "./PlotSim_SampFrac -[d,g,s][target]" << std::endl;
-  std::cout << "  selects target: D | C | Fe | Pb | All" << std::endl;
-  std::cout << std::endl;  
+  std::cout << "./CheckSampFrac -[d,s][target]" << std::endl;
+  std::cout << "  selects target: C | Fe | Pb" << std::endl;
+  std::cout << std::endl;
+  std::cout << "./CheckSampFrac -r[rn]" << std::endl;
+  std::cout << "  selects run number" << std::endl;
+  std::cout << std::endl;
 }
 
 void printOptions() {
-  std::cout << "Executing PlotSim_SampFrac program. The chosen parameters are:" << std::endl;
+  std::cout << "Executing CheckSampFrac program. The chosen parameters are:" << std::endl;
   std::cout << "  targetOption = " << targetOption << std::endl;
-  std::cout << "  dataOption   = " << dataOption << std::endl;
+  std::cout << "  runNumber    = " << runNumber << std::endl;
   std::cout << std::endl;
 }
 
 void assignOptions() {
   // for kind of data
   if (dataFlag) {
-    dataOption = "data";
     titleDraw = " Data";
-    // for targets
-    if (targetOption == "D") {
-      cutTargType = "TargType == 1";
-      inputFile1 = dataDir + "/C/comb_dataC.root";
-      inputFile2 = dataDir + "/Fe/comb_dataFe.root";
-      inputFile3 = dataDir + "/Pb/comb_dataPb.root";
-    } else if (targetOption == "C") {
-      cutTargType = "TargType == 2";
-      inputFile1 = dataDir + "/C/comb_dataC.root";
-    } else if (targetOption == "Fe") {
-      cutTargType = "TargType == 2";
-      inputFile1 = dataDir + "/Fe/comb_dataFe.root";
-    } else if (targetOption == "Pb") {
-      cutTargType = "TargType == 2";
-      inputFile1 = dataDir + "/Pb/comb_dataPb.root";
-    } else if (targetOption == "All") {
-      cutTargType = "TargType == 1 || TargType == 2";
-      inputFile1 = dataDir + "/C/comb_dataC.root";
-      inputFile2 = dataDir + "/Fe/comb_dataFe.root";
-      inputFile3 = dataDir + "/Pb/comb_dataPb.root";
-    }
+    inputFile1 = dataDir + "/" + targetOption + "/comb_data" + targetOption + ".root";
+  } else {
+    titleDraw = " Simrec";
+    inputFile1 = "comb" + targetOption + "_" + runNumber + ".root";
   }
+  // for outdir
+  if (!dataFlag) outDir = ".";
   // names
-  plotFile = outDir + "/samp-frac-" + targetOption + "_" + dataOption + ".png";
+  plotFile = outDir + "/samp-frac-" + targetOption + ".png";
+  rootFile = outDir + "/samp-frac-" + targetOption + "_" + runNumber + ".root";
 }
