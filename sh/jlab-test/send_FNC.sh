4b391ee5a883b20fc415d384a019086ec4be4f43
Andrés Bórquez <andres.borquezc@gmail.com>
2020-09-29 02:02:39 -0300

    FNC: the revenge - part 5

--- sh/jlab-test/send_FNC.sh
+++ sh/jlab-test/send_FNC.sh
@@ -76,32 +76,32 @@ for file in $DATADIR/*.root; do
 
     echo ${jobname}
 
-    # echo "<Request>"                                                                   > ${jobfile}
-    # echo "  <Email email=\"${jobemail}\" request=\"true\" job=\"true\"/>"             >> ${jobfile}
-    # echo "  <Project name=\"${jobproject}\"/>"                                        >> ${jobfile}
-    # echo "  <Track name=\"${jobtrack}\"/>"                                            >> ${jobfile}
-    # echo "  <OS name=\"${jobos}\"/>"                                                  >> ${jobfile}
-    # echo "  <Name name=\"${jobname}\"/>"                                              >> ${jobfile}
-    # echo "  <TimeLimit time=\"${jobtime}\" unit=\"hours\"/>"                          >> ${jobfile}
-    # echo "  <DiskSpace space=\"${jobspace}\" unit=\"GB\"/>"                           >> ${jobfile}
-    # echo "  <Memory space=\"${jobmemory}\" unit=\"MB\"/>"                             >> ${jobfile}
-    # echo "  <CPU core=\"1\"/>"                                                        >> ${jobfile}
-    # echo "  <Input src=\"${thebinary}\"  dest=\"FilterNCombine_sim\"/>"               >> ${jobfile}
-    # echo "  <Input src=\"${execfile}\"   dest=\"run_FNC.sh\"/>"                       >> ${jobfile}
-    # echo "  <Input src=\"${inrootfile}\" dest=\"pruned${tarName}_${id}.root\"/>"      >> ${jobfile}
-    # echo "  <Command><![CDATA["                                                       >> ${jobfile}
-    # echo "    sed -i \"s|^tarName=|tarName=${tarName}|g\" run_FNC.sh"                 >> ${jobfile}
-    # echo "    sed -i \"s|^id=|id=${id}|g\" run_FNC.sh"                                >> ${jobfile}
-    # echo "    chmod 755 ./run_FNC.sh"                                                 >> ${jobfile}
-    # echo "    sh run_FNC.sh"                                                          >> ${jobfile}
-    # echo "  ]]></Command>"                                                            >> ${jobfile}
-    # echo "  <Output src=\"comb${tarName}_${id}.root\" dest=\"${outrootfile}\" />"     >> ${jobfile}
-    # echo "  <Stdout dest=\"${LOGDIR}/${jobname}.out\"/>"                              >> ${jobfile}
-    # echo "  <Stderr dest=\"${LOGDIR}/${jobname}.err\"/>"                              >> ${jobfile}
-    # echo "  <Job>"                                                                    >> ${jobfile}
-    # echo "  </Job>"                                                                   >> ${jobfile}
-    # echo "</Request>"                                                                 >> ${jobfile}
-    # 
-    # echo "Submitting job: ${jobfile}"
-    # jsub --xml ${jobfile}
+    echo "<Request>"                                                                   > ${jobfile}
+    echo "  <Email email=\"${jobemail}\" request=\"true\" job=\"true\"/>"             >> ${jobfile}
+    echo "  <Project name=\"${jobproject}\"/>"                                        >> ${jobfile}
+    echo "  <Track name=\"${jobtrack}\"/>"                                            >> ${jobfile}
+    echo "  <OS name=\"${jobos}\"/>"                                                  >> ${jobfile}
+    echo "  <Name name=\"${jobname}\"/>"                                              >> ${jobfile}
+    echo "  <TimeLimit time=\"${jobtime}\" unit=\"hours\"/>"                          >> ${jobfile}
+    echo "  <DiskSpace space=\"${jobspace}\" unit=\"GB\"/>"                           >> ${jobfile}
+    echo "  <Memory space=\"${jobmemory}\" unit=\"MB\"/>"                             >> ${jobfile}
+    echo "  <CPU core=\"1\"/>"                                                        >> ${jobfile}
+    echo "  <Input src=\"${thebinary}\"  dest=\"FilterNCombine_sim\"/>"               >> ${jobfile}
+    echo "  <Input src=\"${execfile}\"   dest=\"run_FNC.sh\"/>"                       >> ${jobfile}
+    echo "  <Input src=\"${inrootfile}\" dest=\"pruned${tarName}_${id}.root\"/>"      >> ${jobfile}
+    echo "  <Command><![CDATA["                                                       >> ${jobfile}
+    echo "    sed -i \"s|^tarName=|tarName=${tarName}|g\" run_FNC.sh"                 >> ${jobfile}
+    echo "    sed -i \"s|^id=|id=${id}|g\" run_FNC.sh"                                >> ${jobfile}
+    echo "    chmod 755 ./run_FNC.sh"                                                 >> ${jobfile}
+    echo "    sh run_FNC.sh"                                                          >> ${jobfile}
+    echo "  ]]></Command>"                                                            >> ${jobfile}
+    echo "  <Output src=\"comb${tarName}_${id}.root\" dest=\"${outrootfile}\" />"     >> ${jobfile}
+    echo "  <Stdout dest=\"${LOGDIR}/${jobname}.out\"/>"                              >> ${jobfile}
+    echo "  <Stderr dest=\"${LOGDIR}/${jobname}.err\"/>"                              >> ${jobfile}
+    echo "  <Job>"                                                                    >> ${jobfile}
+    echo "  </Job>"                                                                   >> ${jobfile}
+    echo "</Request>"                                                                 >> ${jobfile}
+    
+    echo "Submitting job: ${jobfile}"
+    jsub --xml ${jobfile}
 done
