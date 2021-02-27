d0bcf512a86b09f5119a3b29eee3877bcd1e567d
Andrés Bórquez <andres.borquezc@gmail.com>
2021-02-27 01:26:04 -0300

    Reductor: now it can reduce SimRec and MC (in a special way)

--- Reductor/include/UX.hxx
+++ Reductor/include/UX.hxx
@@ -9,6 +9,12 @@
 
 /*** Options/global variables ***/
 
+TString gDataKind = "data";  // default
+TString gTreeName;
+
+Int_t gReduceSimrec = 0;
+Int_t gReduceMC = 0;
+
 TString gTargetOption;
 TString gRunNumber;
 
@@ -29,6 +35,10 @@ void printUsage() {
   std::cout << "./Reductor -r[run number]" << std::endl;
   std::cout << "    selects run number" << std::endl;
   std::cout << std::endl;
+  std::cout << "./Reductor -s[int]" << std::endl;
+  std::cout << "    reduce simulations instead of data" << std::endl;
+  std::cout << "    0 to reduce MC, 1 to reduce simrec" << std::endl;
+  std::cout << std::endl;
 }
 
 void parseCommandLine(int argc, char *argv[]) {
@@ -37,7 +47,7 @@ void parseCommandLine(int argc, char *argv[]) {
     std::cerr << "Empty command line. Execute ./bin/Reductor -h to print usage." << std::endl;
     exit(1);
   }
-  while ((c = getopt(argc, argv, "ht:r:")) != -1) switch (c) {
+  while ((c = getopt(argc, argv, "ht:r:s:")) != -1) switch (c) {
       case 'h':
         printUsage();
         exit(0);
@@ -48,6 +58,11 @@ void parseCommandLine(int argc, char *argv[]) {
       case 'r':
         gRunNumber = optarg;
         break;
+      case 's':
+        gDataKind = "sim";
+	gReduceMC = 1*(atoi(optarg) == 0) + 0*(atoi(optarg) != 0);
+	gReduceSimrec = 0*(atoi(optarg) == 0) + 1*(atoi(optarg) != 0);
+        break;
       default:
         std::cerr << "Unrecognized argument. Execute ./bin/Reductor -h to print usage." << std::endl;
         exit(0);
@@ -56,14 +71,22 @@ void parseCommandLine(int argc, char *argv[]) {
 }
 
 void assignOptions() {
-  gInputFile  = "pruned" + gTargetOption + "_" + gRunNumber + ".root";
-  gOutputFile = "pruned" + gTargetOption + "_" + gRunNumber + "_red.root";
+  TString sufix = "";
+  if (gReduceMC) sufix = "_MC";
+  gTreeName = "ntuple_" + gDataKind;
+  gInputFile = "pruned" + gTargetOption + "_" + gRunNumber + ".root";
+  gOutputFile = "pruned" + gTargetOption + "_" + gRunNumber + "_red" + sufix + ".root";
 }
 
 void printOptions() {
   std::cout << "Executing Reductor program. The chosen parameters are: " << std::endl;
   std::cout << "  gTargetOption   = " << gTargetOption << std::endl;
   std::cout << "  gRunNumber      = " << gRunNumber << std::endl;
+  std::cout << "  gDataKind       = " << gDataKind << std::endl;
+  if (gDataKind == "sim") {
+    std::cout << "  gReduceMC       = " << gReduceMC << std::endl;
+    std::cout << "  gReduceSimrec   = " << gReduceSimrec << std::endl;
+  }
   std::cout << std::endl;
 }
 
--- Reductor/src/Reductor.cxx
+++ Reductor/src/Reductor.cxx
@@ -13,38 +13,46 @@
 #include "GSTtree.h"
 #include "Reductor.hxx"
 
-/*** Declaration of functions ***/
-
 int main(int argc, char **argv) {
 
   parseCommandLine(argc, argv);
   assignOptions();
   printOptions();
 
+  /*** DATA STRUCTURES ***/
+
+  // sim_p is the same as data_p + gsim variables
+  sim_p spInput;
+  sim_p spOutput;
+
   /*** INPUT ***/
 
-  // define data structure
-  data_p dpInput;
-
-  // define tree
   TChain *tInput = new TChain();
-  tInput->Add(gInputFile + "/ntuple_data");
+  tInput->Add(gInputFile + "/" + gTreeName);
   tInput->SetBranchStatus("*", 1);
-  SetParticleBranches_Input(tInput, dpInput);
-  
+  SetParticleBranches_Input(tInput, spInput);
+
   /*** OUTPUT ***/
 
-  // define file
   TFile *rootFile = new TFile(gOutputFile, "RECREATE", "Data of particles");
+  TTree *tOutput = new TTree(gTreeName, "Stable particles");
+  SetParticleBranches_Output(tOutput, spOutput);
 
-  // define data structure
-  data_p dpOutput;
-  
-  // define tree
-  TTree *tOutput = new TTree("ntuple_data", "Stable particles");
-  SetParticleBranches_Output(tOutput, dpOutput);
-  
-  /*** START ***/
+  // for gsim
+  Int_t iPip, iPim, iPi0;
+  TTree *tPip;
+  TTree *tPim;
+  TTree *tPi0;
+  if (gReduceMC) {
+    tPip = new TTree("tPip", "Positive Pions");
+    tPim = new TTree("tPim", "Negative Pions");
+    tPi0 = new TTree("tPi0", "Neutral Pions");
+    tPip->Branch("iPip", &iPip);
+    tPim->Branch("iPim", &iPim);
+    tPi0->Branch("iPi0", &iPi0);
+  }
+
+  /*** DECLARATIONS ***/
 
   // define particle counters
   Int_t nPip = 0;
@@ -60,22 +68,25 @@ int main(int argc, char **argv) {
   // loop around events (it says entries, but they are really events)
   Int_t currentEvent, previousEvent;
   Int_t nEntries = tInput->GetEntries();
-  
+
 #ifdef DEBUG
   nEntries = 10000;
 #endif
-  
+
+  /*** START ***/
+
+  Int_t k = 0; // counter of output entries
   for (Int_t i = 0; i <= nEntries; i++) {
     tInput->GetEntry(i);
-    currentEvent = (Int_t)dpInput.evnt;
+    currentEvent = (Int_t)spInput.evnt;
 
     // prevents repetition of same event
     if (i > 0) {
       tInput->GetEntry(i - 1);
-      previousEvent = (Int_t)dpInput.evnt;
+      previousEvent = (Int_t)spInput.evnt;
       if (previousEvent == currentEvent) continue;
     }
-    
+
 #ifdef DEBUG
     std::cout << "Current event number: " << currentEvent << std::endl;
     std::cout << "Current entry number: " << i << std::endl;
@@ -86,50 +97,80 @@ int main(int argc, char **argv) {
       tInput->GetEntry(j);
 
       // same-event condition
-      if (currentEvent == (Int_t)dpInput.evnt) {
+      if (currentEvent == (Int_t)spInput.evnt) {
 #ifdef DEBUG
-	std::cout << "Current pid:          " << dpInput.pid << std::endl;
+        if (gReduceSimrec || gDataKind == "data") std::cout << "Current pid:          " << spInput.pid << std::endl;
+	else if (gReduceMC) std::cout << "Current mc_pid:       " << spInput.mc_pid << std::endl;
 #endif
         // count the particles
-        if ((Int_t)dpInput.pid == 211) {
-          nPip++;
-	  pospions.push_back(j);
-	} else if ((Int_t)dpInput.pid == -211) {
-          nPim++;
-	  negpions.push_back(j);
-	} else if ((Int_t)dpInput.pid == 22) {
-	  nGamma++;
-	  gammas.push_back(j);
+        if (gReduceSimrec || gDataKind == "data") {
+          if ((Int_t)spInput.pid == 211) {
+            nPip++;
+            pospions.push_back(j);
+          } else if ((Int_t)spInput.pid == -211) {
+            nPim++;
+            negpions.push_back(j);
+          } else if ((Int_t)spInput.pid == 22) {
+            nGamma++;
+            gammas.push_back(j);
+          }
+        } else if (gReduceMC) {
+          if ((Int_t)spInput.mc_pid == 211) {
+            nPip++;
+            pospions.push_back(j);
+          } else if ((Int_t)spInput.mc_pid == -211) {
+            nPim++;
+            negpions.push_back(j);
+          } else if ((Int_t)spInput.mc_pid == 22) {
+            nGamma++;
+            gammas.push_back(j);
+          }
         }
       } else {
         break;
-      } // end of same-event condition
-      
-    } // end loop of particles in current event
+      }  // end of same-event condition
+
+    }  // end loop of particles in current event
 
     // arrange all particles in entries vector
     entries.insert(entries.end(), pospions.begin(), pospions.end());
     entries.insert(entries.end(), negpions.begin(), negpions.end());
     entries.insert(entries.end(), gammas.begin(), gammas.end());
-    
+
 #ifdef DEBUG
     std::cout << "N = (" << nPip << ", " << nPim << ", " << nGamma << ")" << std::endl;
 #endif
-    
+
     /*** MINIMUM SET OF PARTICLES FOUND ***/
 
     if (nPip >= 1 && nPim >= 1 && nGamma >= 2) {
+      Int_t iGamma = 1;
       for (Size_t j = 0; j < entries.size(); j++) {
-	tInput->GetEntry(entries[j]); // update all dpInput values
-	dpOutput = dpInput;
-	tOutput->Fill();
+        tInput->GetEntry(entries[j]);  // update all spInput values
+        spOutput = spInput;
+        tOutput->Fill();
+        // fill the other trees
+        if (gReduceMC) {
+          if (spInput.mc_pid == 211) {
+            iPip = k;
+            tPip->Fill();
+          } else if (spInput.mc_pid == -211) {
+            iPim = k;
+            tPim->Fill();
+          } else if (spInput.mc_pid == 22 && iGamma < nGamma) {
+            iGamma++;
+            iPi0 = k;
+            tPi0->Fill();
+          }
+        }
+	k++; // update output entries
       }
     }
-    
+
 #ifdef DEBUG
-        std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
 #endif
-    
+
     // reset particle counters
     nPip = 0;
     nPim = 0;
@@ -140,15 +181,14 @@ int main(int argc, char **argv) {
     negpions.clear();
     gammas.clear();
     entries.clear();
-  } // end of loop in events
+  }  // end of loop in events
 
   /*** WRITE ***/
-  
+
   rootFile->Write();
   rootFile->Close();
-  
+
   std::cout << "File " << gOutputFile << " has been created!" << std::endl;
 
   return 0;
 }
-
