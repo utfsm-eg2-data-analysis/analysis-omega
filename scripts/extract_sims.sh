4abc5ceedf2206f722887513e7757cc322902333
Andrés Bórquez <andres.borquezc@gmail.com>
2021-09-09 17:29:50 -0300

    Added scripts to manage simulations

---
+++ scripts/extract_sims.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+############################################
+#                                          #
+#  Script made to extract simulations,     #
+#      from volatile/ to work/             #
+#                                          #
+############################################
+
+function print_help() {
+    echo "SCRIPT: extract_sims.sh"
+    echo "======================="
+    echo "./extract_sims.sh --part <part> --targ <targ> --run1 <run1> --run2 <run2>"
+    echo "Where:"
+    echo "  <part> = selects particle (eta, omega)"
+    echo "  <targ> = selects target (D, C, Fe, Pb)"
+    echo "  <run1> = first run number to loop over"
+    echo "  <run2> = last run number to loop over"
+    echo "Example:"
+    echo "  ./extract_sims.sh --part omega --targ D --run1 1 --run2 2500"
+    exit
+}
+
+function process_args() {
+    arr=("$@")
+    ic=0
+    while [[ $ic -le $((${#arr[@]}-1)) ]]; do
+        if [[ "${arr[$ic]}" == "--part" ]]; then
+            part=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--targ" ]]; then
+            targ=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--run1" ]]; then
+            run1=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--run2" ]]; then
+            run2=${arr[$((ic+1))]}
+        else
+            echo "ERROR: unrecognized argument: ${arr[$((ic))]}.";
+            print_help;
+        fi
+        ((ic+=2))
+    done
+}
+
+function print_args() {
+    echo "SCRIPT: extract_sims.sh"
+    echo "======================="
+    echo "part  = ${part}"
+    echo "targ  = ${targ}"
+    echo "run1  = ${run1}"
+    echo "run2  = ${run2}"
+}
+
+
+function get_number_4dig() {
+    sr=$1
+    srn=""
+    if [[ ${sr} -lt 10 ]]; then
+        srn="000${sr}"
+    elif [[ ${sr} -lt 100 ]]; then
+        srn="00${sr}"
+    elif [[ ${sr} -lt 1000 ]]; then
+        srn="0${sr}"
+    else
+        srn="${sr}"
+    fi
+    echo ${srn}
+}
+
+################
+###   Main   ###
+################
+
+if [[ -z "${VLTLDIR}" ]]; then
+    echo "ERROR: variable VLTLDIR is unset."
+    exit
+fi
+
+if [[ -z "${WORKDIR}" ]]; then
+    echo "ERROR: variable WORKDIR is unset."
+    exit
+fi
+
+NARGS=8
+if [[ ${#} -ne ${NARGS} ]]; then
+    echo "ERROR: ${#} arguments were provided, they should be ${NARGS}."
+    print_help
+fi
+
+argArray=("$@")
+process_args "${argArray[@]}"
+print_args
+
+in_path=${VLTLDIR}/${part}Sim/farm/${targ}
+out_path=${WORKDIR}/${part}Sim/${targ}
+
+for ((i=${run1}; i<=${run2}; i++)); do
+    string_i="$(get_number_4dig ${i})"
+    mkdir -vp ${out_path}/${string_i:0:2}/
+    mv -v ${in_path}/run${string_i}/clastool/recsis${targ}.root ${out_path}/${string_i:0:2}/recsisD_${string_i:2:2}.root
+done
---
+++ scripts/merge_sims.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+############################################
+#                                          #
+#  Script made to merge extracted          #
+#       simulations in work/               #
+#                                          #
+############################################
+
+function print_help() {
+    echo "SCRIPT: merge_sims.sh"
+    echo "======================="
+    echo "./merge_sims.sh --part <part> --targ <targ>"
+    echo "Where:"
+    echo "  <part> = selects particle (eta, omega)"
+    echo "  <targ> = selects target (D, C, Fe, Pb)"
+    echo "Example:"
+    echo "  ./merge_sims.sh --part omega --targ D"
+    exit
+}
+
+function process_args() {
+    arr=("$@")
+    ic=0
+    while [[ $ic -le $((${#arr[@]}-1)) ]]; do
+        if [[ "${arr[$ic]}" == "--part" ]]; then
+            part=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--targ" ]]; then
+            targ=${arr[$((ic+1))]}
+        else
+            echo "ERROR: unrecognized argument: ${arr[$((ic))]}.";
+            print_help;
+        fi
+        ((ic+=2))
+    done
+}
+
+function print_args() {
+    echo "SCRIPT: merge_sims.sh"
+    echo "====================="
+    echo "part  = ${part}"
+    echo "targ  = ${targ}"
+}
+
+
+function get_number_4dig() {
+    sr=$1
+    srn=""
+    if [[ ${sr} -lt 10 ]]; then
+        srn="000${sr}"
+    elif [[ ${sr} -lt 100 ]]; then
+        srn="00${sr}"
+    elif [[ ${sr} -lt 1000 ]]; then
+        srn="0${sr}"
+    else
+        srn="${sr}"
+    fi
+    echo ${srn}
+}
+
+################
+###   Main   ###
+################
+
+if [[ -z "${WORKDIR}" ]]; then
+    echo "ERROR: variable WORKDIR is unset."
+    exit
+fi
+
+NARGS=4
+if [[ ${#} -ne ${NARGS} ]]; then
+    echo "ERROR: ${#} arguments were provided, they should be ${NARGS}."
+    print_help
+fi
+
+argArray=("$@")
+process_args "${argArray[@]}"
+print_args
+
+in_path=${WORKDIR}/${part}Sim/${targ}
+
+for dir in ${in_path}/*/; do
+    dir_number=${dir:(-3):2} # slice path from right to left
+    hadd ${in_path}/recsisD_${dir_number}.root ${dir}/recsis${targ}_*.root
+done
